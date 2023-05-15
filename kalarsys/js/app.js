702924f10d403b6e8391629cb4b82a0fb16cad78
Andrew Lingenfelter <luridsorcerer@gmail.com>
2023-05-15 09:54:29 -0400

    Added more Kalarsys backgrounds, randomly selected

--- kalarsys/js/app.js
+++ kalarsys/js/app.js
@@ -1,1 +1,13 @@
-console.log("JS loaded");
+// list of background images
+let backgrounds = [
+    "img/bg_0.png",
+    "img/bg_1.png",
+    "img/bg_2.png",
+    "img/bg_3.png"
+];
+
+// select which backgound should be used
+let i = Math.floor( Math.random()*backgrounds.length );
+
+// apply the background image
+document.body.style.backgroundImage = "url('"+backgrounds[i]+"')";
